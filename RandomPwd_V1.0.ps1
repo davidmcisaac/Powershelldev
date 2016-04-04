08e1be30443351f856ccf7426333280f31fe3976
mcisaacit <github@mcisaacit.com>
2016-04-04 16:32:18 +0100

    Update RandomPwd_V1.0.ps1

--- RandomPwd_V1.0.ps1
+++ RandomPwd_V1.0.ps1
@@ -1,3 +1,6 @@
-﻿#source http://blogs.technet.com/b/poshchap/archive/2016/01/29/use-a-foreach-loop-in-parameter-validation.aspx
-#current sets random password length to 12, (second section is for the number of alpa numeric characters)
+<#
+source http://blogs.technet.com/b/poshchap/archive/2016/01/29/use-a-foreach-loop-in-parameter-validation.aspx
+current sets random password length to 12, (second section is for the number of alpa numeric characters)
+Going to use this code as part of password reset tool for AD users
+#>
 Add-Type -AssemblyName System.web;[System.Web.Security.Membership]::GeneratePassword(12,5)
