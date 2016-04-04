c54cce6b77c31e6d90e24061659e87f7f5386d23
mcisaacit <github@mcisaacit.com>
2016-04-04 13:49:00 +0100

    Generate random password
    http://blogs.technet.com/b/poshchap/archive/2016/01/29/use-a-foreach-loop-in-parameter-validation.aspx

---
+++ RandomPwd_V1.0.ps1
@@ -0,0 +1,3 @@
+﻿#source http://blogs.technet.com/b/poshchap/archive/2016/01/29/use-a-foreach-loop-in-parameter-validation.aspx
+#current sets random password length to 12, (second section is for the number of alpa numeric characters)
+Add-Type -AssemblyName System.web;[System.Web.Security.Membership]::GeneratePassword(12,5)
