e6d9805d6480df3362824488045308ef4a4c138b
mcisaacit <github@mcisaacit.com>
2016-04-05 08:52:57 +0100

    Fun with strings
    Playing with stings with aim to learning how to carve them up to be used
    for passing onto other functions

---
+++ fun_with_strings.ps1
@@ -0,0 +1,9 @@
+﻿$domain = "ou=abc,ou=level 2,dc=level 3,dc=scottish,dc=mci,dc=local"
+$a = @($domain)
+
+$alength = ([regex]::Matches($a, "," )).count
+$areallen = @{expression=($alength - 1)}
+
+$b = $domain -split ","
+write-host " array created"
+$b[-1]
